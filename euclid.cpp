0e4829107e08098aa513c3053f6288fc7c8c7809
yegorf1 <yegorf1@gmail.com>
2015-07-21 13:06:37 +0300

    Added gdc and lnc algos

---
+++ euclid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+long gcd(long a, long b) {
+    while (b) {
+        long r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+long lnc(long a, long b) {
+    return a (b / gcd(a, b));
+}
+
+int main () {
+    return 0;
+}
